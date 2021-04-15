5bd556a4351b90765972da534e31252f80942336
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-15 19:34:58 -0300

    Alura Course - Collections 2 - Using LINQ

---
+++ Month.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaCollections
+{
+    class Month
+    {
+        public string Name { get; private set; }
+        public int Days { get; private set; }
+
+        public Month(string name, int days)
+        {
+            Name = name;
+            Days = days;
+        }
+
+        public override string ToString()
+        {
+            return $"Month: {Name.PadRight(12)}{Days} days";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Month> months = new List<Month>
+                {
+                    new Month("Janeiro", 31),
+                    new Month("Janeiro", 31),
+                    new Month("Fevereiro", 28),
+                    new Month("Março", 31),
+                    new Month("Abril", 30),
+                    new Month("Maio", 31),
+                    new Month("Junho", 30),
+                    new Month("Julho", 31),
+                    new Month("Agosto", 31),
+                    new Month("Setembro", 30),
+                    new Month("Outubro", 31),
+                    new Month("Novembro", 30),
+                    new Month("Dezembro", 31)
+                };
+            PrintMonths(months);
+
+            Console.WriteLine("\nLinq\n");
+
+            var listLinq = months.Where(m => m.Days == 31).OrderBy(m => m.Name);
+            var names = months.Where(m => m.Days == 31).OrderBy(m => m.Name).Select(m => m.Name);
+            var days = months.Where(m => m.Days == 31).OrderBy(m => m.Name).Select(m => m.Days);
+
+            //IEnumerable<Month> listLinq = months.Where(m => m.Days == 31).OrderBy(m => m.Name);
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            PrintMonths(listLinq);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintMonths(IEnumerable<Month> months)
+        {
+            Console.WriteLine();
+            foreach (var month in months)
+            {
+                Console.WriteLine(month);
+            }
+        }
+    }
+}
